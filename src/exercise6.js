e250a13a9741bd0cc3f9ade572b3e748dd5ea00a
teresa0613 <teresa1306@hotmail.com>
2020-09-30 17:15:39 +0200

    ex6 complete

---
+++ src/exercise6.js
@@ -0,0 +1,20 @@
+import express from 'express'
+
+//Add a route /oddtestwhich will take a number as a parameter.
+//A message must be returned to indicate that the number passed in parameter is odd or even
+
+const PORT = 4000
+const app = express()
+
+app.get('/oddtest/:number', (req, res) => {
+    let number = req.params.number
+    if (number % 2 === 0) {
+        res.send('You found an even number')
+    } else {
+        res.send('You found an odd number')
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
+})

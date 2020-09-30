3d5a2658ccf5a16cab1d313cf848792a0fc667e2
teresa0613 <teresa1306@hotmail.com>
2020-09-30 17:08:56 +0200

    exercise5.js complete

---
+++ src/exercise5.js
@@ -0,0 +1,20 @@
+import express from 'express'
+
+//Add a route /palindromewhich will take a word as a parameter.
+//A message should be returned to the user if the word is a palindrome.
+
+const PORT = 2000
+const app = express()
+
+app.get('/palindrome/:word', (req, res) => {
+    const input = req.params.word
+    if (input === input.split('').reverse().join('')) {
+        res.send(`You found a palindrome`)
+    } else {
+        res.send('Try again, not a palindrome')
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
+})

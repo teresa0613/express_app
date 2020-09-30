3daa94b52ced59ce9ef3050dab82068dcf39d5c2
teresa0613 <teresa1306@hotmail.com>
2020-09-30 15:29:17 +0200

    exercise 1 without notes

--- src/app.js
+++ src/app.js
@@ -7,19 +7,12 @@
 import express from 'express'
 const app = express()
 
-// the listening port of our server
 const PORT = 7777
 
-// definition of a route '/', the route by default.
-// when a client makes a request on this endpoint
-// we will return the text 'Hello World!' via the callback /
-// This callback is also called the 'handler function'
 app.get('/', (req, res) => {
     res.send('Express exercises part 1')
 })
 
-// start our server on port 3000
 app.listen(PORT, () => {
-    // run a display when starting the server.
     console.log(`Example app listening at http://localhost:${PORT}`)
 })
